4ba6fa967491fe2f90693e409f0124ece8650385
ZozeLuis <paodebatataquerocafe@gmail.com>
2025-05-22 17:45:42 -0300

    app 2 conexao com o firebase

---
+++ app/src/main/java/com/example/segurappgestao/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.segurappgestao // Make sure your package name is correct
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ListenerRegistration // Import for real-time listener
+
+class MainActivity : AppCompatActivity() {
+
+
+}
